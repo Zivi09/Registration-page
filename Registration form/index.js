0bcc8cee8c3831088c34e65a16885cd46c060cef
Ziven Pereira <pereirazivencs232417@gmail.com>
2024-09-28 09:32:20 +0530

    Add files via upload

---
+++ Registration form/index.js
@@ -0,0 +1,17 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+    login.classList
+});
+
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+    login.classList.slider("reg page 1.html")
+    
+});
